3d6036c10bc9ab3345c94484f5a4cb8c1d1ad477
joashcabanilla <joash0199@gmail.com>
2022-02-08 06:45:05 +0800

    update homepage

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import cssHome from '../styles/Pages/Home.module.css'
 import { Container, Row, Col } from 'react-bootstrap';
 import Navbar from '../components/navbar';
-import Search from '../components/search';
 import Image from 'next/image';
 
 export default function Home() {
@@ -13,8 +12,14 @@ export default function Home() {
           <Col className={cssHome.imageCol}>
             <Image className={cssHome.imageBackground} src='/image/cover_02.png' alt='Resortex Logo' layout='fill' quality={100} priority />
           </Col>
+          <div className={cssHome.divTagline}>
+            <h1>It’s not a hotel,</h1>
+            <h1>it’s a way of life.</h1>
+            <p>The art of meeting your highest expectations.</p>
+          </div>
+          <div className={cssHome.divSearch}>
+          </div>
         </Row>
-        <Search />
       </Container>
     </>
   )
