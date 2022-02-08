f1c798de97331f5c5d3acf537930aadd62acc1ea
joashcabanilla <joash0199@gmail.com>
2022-02-08 11:33:39 +0800

    update home

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
             <Image className={cssHome.imageBackground} src='/image/cover_02.png' alt='Resortex Logo' layout='fill' quality={100} priority />
           </Col>
           <div className={cssHome.divTagline}>
-            <h1>It’s not a hotel,</h1>
+            <h1>It’s not just a hotel,</h1>
             <h1>it’s a way of life.</h1>
             <p>The art of meeting your highest expectations.</p>
           </div>
