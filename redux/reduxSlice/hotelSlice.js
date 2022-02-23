9b3d3c47b754aaaeb76bab941c734a21b8bb0c6a
joashcabanilla <joash0199@gmail.com>
2022-02-23 15:39:24 +0800

    update redux reducers

--- redux/reduxSlice/hotelSlice.js
+++ redux/reduxSlice/hotelSlice.js
@@ -1,9 +1,14 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import { database } from '../../firebase/firebaseConfig';
 import { ref, child, get } from 'firebase/database';
 
-const hotelRef = ref(database)
-let databasePath = "";
+// const hotelRef = ref(database)
+// let databasePath = "";
+
+//FIREBASE FUNCTIONS 
+export const getHotel = createAsyncThunk(
+    'hotel/getStatus'
+);
 
 const initialState = {
     hotel: [],
@@ -13,23 +18,24 @@ export const hotelSlice = createSlice({
     name: 'hotel',
     initialState,
     reducers: {
-        getHotelData: (state) => {
-            getHotel();
-        },
     },
+    extraReducers: {
+
+    }
 });
 
 //FIREBASE FUNCTIONS 
-const getHotel = () => {
-    databasePath = 'HOTEL-RESERVATION-SYSTEM/HOTELS';
-    get(child(hotelRef, databasePath))
-        .then((snapshot) => {
-            snapshot.exists() ? console.log(snapshot.val()) : null;
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-}
+// const getHotel = () => {
+//     databasePath = 'HOTEL-RESERVATION-SYSTEM/HOTELS';
+//     get(child(hotelRef, databasePath))
+//         .then((snapshot) => {
+//             snapshot.exists() ? console.log(snapshot.val()) : null;
+//         })
+//         .catch((err) => {
+//             console.log(err);
+//         });
+// }
+
 export const { getHotelData } = hotelSlice.actions;
 
 export default hotelSlice.reducer;
