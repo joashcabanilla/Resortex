9d114722feca0350154af96dc1d3bed42fe05c2e
joashcabanilla <joash0199@gmail.com>
2022-03-12 02:21:48 +0800

    initial sign in UI

--- components/home/navbar.jsx
+++ components/home/navbar.jsx
@@ -1,12 +1,19 @@
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, Modal, Button } from 'react-bootstrap';
 import Image from 'next/image';
 import Link from 'next/link';
 import cssNavbar from '../../styles/Components/Navbar.module.css';
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
+import css from '../../styles/Components/modalSignIn.module.css';
 
 export default function navbar() {
+    //state declaration
     const [navbar, setNavbar] = useState(false);
+    const [expanded, setExpanded] = useState(false);
+    const [modalSignInShow,setModalSignInShow] = useState(false);
+    const [modalCustomerShow,setModalCustomerShow] = useState(false);
+    const [modalHotelShow,setModalHotelShow] = useState(false);
 
+    //react Hooks
     useEffect(() => {
         window.addEventListener('scroll', onScroll);
     }, []);
@@ -15,9 +22,60 @@ export default function navbar() {
         window.scrollY >= 100 ? setNavbar(true) : setNavbar(false);
     }
 
+    //event handlers functions
+
+    //components
+    const customerModal = () => {
+        return (
+            <Modal show={modalCustomerShow} onHide={()=> setModalCustomerShow(false)} animation={true}  centered>
+                <Modal.Header closeButton className={css.modalCustomerHeader}>
+                    <p>CUSTOMER</p>
+                </Modal.Header>
+    
+                <Modal.Body>
+                </Modal.Body>
+    
+                <Modal.Footer className={css.modalSignInFooter}>
+                </Modal.Footer>
+            </Modal>
+            );
+    }
+    const hotelModal = () => {
+        return(
+            <Modal show={modalHotelShow} onHide={()=> setModalHotelShow(false)} animation={true}  centered>
+                <Modal.Header closeButton className={css.modalSignInHeader}>
+                    <p>HOTEL</p>
+                </Modal.Header>
+    
+                <Modal.Body>
+                </Modal.Body>
+    
+                <Modal.Footer className={css.modalSignInFooter}>
+                </Modal.Footer>
+            </Modal>
+            );
+    }
+    const signInModal = () => {
+        return (
+        <Modal show={modalSignInShow} onHide={()=> setModalSignInShow(false)} animation={true}  centered>
+            <Modal.Header closeButton className={css.modalSignInHeader}>
+                <p>SIGN IN</p>
+            </Modal.Header>
+
+            <Modal.Body>
+            </Modal.Body>
+
+            <Modal.Footer className={css.modalSignInFooter}>
+                <Button onClick={() => {setModalSignInShow(false); setModalCustomerShow(true);}}>CREATE ACCOUNT AS CUSTOMER</Button>
+                <Button onClick={() => {setModalSignInShow(false); setModalHotelShow(true);}}>CREATE ACCOUNT AS HOTEL OWNER</Button>
+            </Modal.Footer>
+        </Modal>
+        );
+    }
+
     return (
         <>
-            <Navbar fixed='top' expand='sm' variant='dark' bg={navbar ? `light` : null} collapseOnSelect className={navbar ? `${cssNavbar[`navbar-scroll`]}` : `${cssNavbar[`navbar`]}`}>
+            <Navbar expanded={expanded} fixed='top' expand='sm' variant='dark' bg={navbar ? `light` : null} collapseOnSelect className={navbar ? `${cssNavbar[`navbar-scroll`]}` : `${cssNavbar[`navbar`]}`}>
                 <Navbar.Brand>
                     <Link href='/'>
                         <a>
@@ -28,29 +86,33 @@ export default function navbar() {
                 <Link href='/'>
                     <a className={navbar ? `${cssNavbar[`title-scroll`]}` : `${cssNavbar[`title`]}`}>Resort Reservation</a>
                 </Link>
-                <Navbar.Toggle aria-controls='responsive-navbar-nav' className='navbar-toggler'>
+                <Navbar.Toggle aria-controls='responsive-navbar-nav' className='navbar-toggler' onClick={() => setExpanded(expanded ? false : "expanded")}>
                     <span className={navbar ? 'toggler-icon-scroll top-bar' : 'toggler-icon top-bar'}></span>
                     <span className={navbar ? 'toggler-icon-scroll middle-bar' : 'toggler-icon middle-bar'}></span>
                     <span className={navbar ? 'toggler-icon-scroll bottom-bar' : 'toggler-icon bottom-bar'}></span>
                 </Navbar.Toggle>
                 <Navbar.Collapse className='justify-content-end' id='responsive-navbar-nav'>
                     <Nav className={navbar ? 'nav-scroll' : null}>
-                        <Nav.Link className={navbar ? 'navLink-scroll' : 'navLink'}>
+                        <Nav.Link className={navbar ? 'navLink-scroll' : 'navLink'} onClick={() => setExpanded(false)}>
                             <Link href='/'>
                                 <p className={navbar ? 'navLinkP-scroll' : 'navLinkP'}>HOME</p>
                             </Link>
                         </Nav.Link>
-                        <Nav.Link className={navbar ? 'navLink-scroll' : 'navLink'}>
+                        <Nav.Link className={navbar ? 'navLink-scroll' : 'navLink'} onClick={() => setExpanded(false)}>
                             <Link href='/about'>
                                 <p className={navbar ? 'navLinkP-scroll' : 'navLinkP'}>ABOUT US</p>
                             </Link>
                         </Nav.Link>
-                        <Nav.Link className={navbar ? 'navLink-scroll' : 'navLink'}>
+                        <Nav.Link className={navbar ? 'navLink-scroll' : 'navLink'} onClick={()=> {setModalSignInShow(true); setExpanded(false);}}>
                             <p className={navbar ? 'navLinkP-scroll' : 'navLinkP'}>SIGN IN</p>
                         </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
+
+            {signInModal()}
+            {customerModal()}
+            {hotelModal()}
         </>
     );
 }
